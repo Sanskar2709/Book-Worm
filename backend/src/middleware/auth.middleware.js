fe55e2642e7c2baef6f02b312041ae5ea09b7f96
Sanskar2709 <sanskartiwari270901@gmail.com>
2025-04-16 18:07:38 +0530

    dsds

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -11,18 +11,18 @@ import User from "../models/User.js";
 // });
 
 const protectRoute = async (req, res, next) => {
+  const token = req.header("Authorization").replace("Bearer", "");
+
   try {
     // get token
-    const token = req.header("Authorization").replace("Bearer", "");
     if (!token)
       return res
         .status(401)
         .json({ message: "No authentication token, access denied" });
 
     //verify token
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     console.log(token, " secret ", process.env.JWT_SECRET);
-
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     //find user
     const user = await User.findById(decoded.userId).select("-password");
     if (!user) return res.status(401).json({ message: "Token is not valid" });
@@ -31,7 +31,11 @@ const protectRoute = async (req, res, next) => {
     next();
   } catch (error) {
     console.log("Authentication error", error.message);
-    res.status(401).json({ message: "Token is not valid" });
+    res
+      .status(401)
+      .json({
+        message: `Token is not valid: ${token}, secret: ${process.env.JWT_SECRET}`,
+      });
   }
 };
 

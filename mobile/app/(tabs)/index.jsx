09fa2a89e04c497723c4e7e66fff5b5c9a87ca4b
Sanskar2709 <sanskartiwari270901@gmail.com>
2025-04-17 01:19:45 +0530

    seds

--- mobile/app/(tabs)/index.jsx
+++ mobile/app/(tabs)/index.jsx
@@ -1,15 +1,25 @@
 import { View, Text, TouchableOpacity } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { useAuthStore } from "../../store/authStore";
+import { useEffect } from "react";
 
 export default function Home() {
-  const { logout } = useAuthStore();
+  const { token } = useAuthStore();
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+
+  const fetchBooks = async (pageNum = 1, refresh = false) => {};
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const handleLoadMore = async () => {};
   return (
     <View>
       <Text>home tab</Text>
-      <TouchableOpacity onPress={logout}>
-        <Text>Logout</Text>
-      </TouchableOpacity>
     </View>
   );
 }

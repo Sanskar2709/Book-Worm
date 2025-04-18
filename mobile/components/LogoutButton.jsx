19130c282d27e6ca05476669f324cba25484965d
Sanskar2709 <sanskartiwari270901@gmail.com>
2025-04-19 03:23:14 +0530

    dzdd

--- mobile/components/LogoutButton.jsx
+++ mobile/components/LogoutButton.jsx
@@ -1,10 +1,19 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
+import { useAuthStore } from "../store/authStore";
+import styles from "../assets/styles/profile.styles";
+import { Ionicons } from "@expo/vector-icons";
+import COLORS from "../constants/colors";
 
 export default function LogoutButton() {
+  const { logout } = useAuthStore();
+
+  const confirmLogout = () => {};
+
   return (
-    <View>
-      <Text>LogoutButton</Text>
-    </View>
+    <TouchableOpacity style={styles.logoutButton} onPress={confirmLogout}>
+      <Ionicons name="log-out-outline" size={20} color={COLORS.white} />
+      <Text style={styles.logoutText}>Logout</Text>
+    </TouchableOpacity>
   );
 }
--- mobile/components/ProfileHeader.jsx
+++ mobile/components/ProfileHeader.jsx
@@ -1,10 +1,24 @@
 import { View, Text } from "react-native";
+import { Image } from "expo-image";
+import { useAuthStore } from "../store/authStore";
+import styles from "../assets/styles/profile.styles";
+import { formatMemberSince } from "../lib/utils";
 import React from "react";
 
 export default function ProfileHeader() {
+  const { user } = useAuthStore();
+
   return (
-    <View>
-      <Text>ProfileHeader</Text>
+    <View style={styles.profileHeader}>
+      <Image source={{ uri: user.profileImage }} style={styles.profileImage} />
+
+      <View style={styles.profileInfo}>
+        <Text style={styles.username}>{user.username}</Text>
+        <Text style={styles.email}>{user.email}</Text>
+        <Text style={styles.memberSince}>
+          🗓️ Joined {formatMemberSince(user.createdAt)}
+        </Text>
+      </View>
     </View>
   );
 }

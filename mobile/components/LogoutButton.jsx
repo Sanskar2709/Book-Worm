3ebd65a018dac68c7b9753b45c8ba758a02a42fe
Sanskar2709 <sanskartiwari270901@gmail.com>
2025-04-19 03:29:09 +0530

    sesrf

--- mobile/components/LogoutButton.jsx
+++ mobile/components/LogoutButton.jsx
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Alert } from "react-native";
 import React from "react";
 import { useAuthStore } from "../store/authStore";
 import styles from "../assets/styles/profile.styles";
@@ -8,7 +8,12 @@ import COLORS from "../constants/colors";
 export default function LogoutButton() {
   const { logout } = useAuthStore();
 
-  const confirmLogout = () => {};
+  const confirmLogout = () => {
+    Alert.alert("Logout", "Are you sure you want to logout?", [
+      { text: "Cancel", style: "cancel" },
+      { text: "Logout", onPress: () => logout(), style: "destructive" },
+    ]);
+  };
 
   return (
     <TouchableOpacity style={styles.logoutButton} onPress={confirmLogout}>
